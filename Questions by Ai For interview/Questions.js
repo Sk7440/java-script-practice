a5e78fb444030a3c692534bc33bf2be385ce8a9b
Sk7440 <sultankhalid1007@gmail.com>
2026-07-10 23:06:09 +0500

    dynamic quiz builder in process

---
+++ Questions by Ai For interview/Questions.js
@@ -0,0 +1,12 @@
+// Write a function that takes a string and returns an object containing the count of every character
+// Write a function to reverse a string
+// write a function to reveser a word in a sentence
+// write a function to find first no repeating character
+// write a function to return count for non repeating characters
+// write a function to  find duplicate characters in string
+// write a function to repeat the first repeated character
+// write a function to wirte the last non repeating character
+
+// write a function to find vowel in a string
+// write  a function to find character with highest frequency
+// function to remove duplicate letters from the string
