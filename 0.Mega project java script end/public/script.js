8b53c00b6ee690297747b0a1ecfc3f5314f7f546
Sk7440 <sultankhalid1007@gmail.com>
2026-09-09 01:13:14 +0500

    mega project done100%

--- 0.Mega project java script end/public/script.js
+++ 0.Mega project java script end/public/script.js
@@ -1,34 +1,140 @@
-import { createAnimatable, utils } from 'animejs';
+//overview section
+let balanceD = document.querySelector(".dbalance");
+let incomeD = document.querySelector(".dincome");
+let expenseD = document.querySelector(".dexpense");
+let expenseA = document.querySelector(".aexpense");
+let balanceA = document.querySelector(".abalance");
+let transactions = document.querySelector(".atransactions");
+//form
+let form = document.querySelector("#transaction-form");
+let description = document.querySelector("#description");
+let amount = document.querySelector("#amount");
+let category = document.querySelector("#category");
+let date = document.querySelector("#date");
+let select = document.querySelector("#genus");
+//transaction list
+let trancastionList = document.querySelector(".transactions-list");
+let trancastionItem = document.querySelector(".transaction-item");
+let resultDesc = document.querySelector(".tx-desc");
+let resultCat = document.querySelector(".tx-cat");
+let resultDate = document.querySelector(".tx-date");
+let resultAmount = document.querySelector(".tx-amount");
+let btnTrans = document.querySelector(".btn");
+// khus sy let kye
+let expensesTotal;
+let incomeTotal;
+let arr = JSON.parse(localStorage.getItem("array")) || [];
+///Search
+let filterSelect = document.querySelector(".filter-select");
+///select logic
+filterSelect.addEventListener("change", () => {
+  if (filterSelect.value == "") {
+    render();
+  } else if (filterSelect.value == "income") {
+    let newarr = arr.filter((ele, idx) => {
+      return ele.type == filterSelect.value;
+    });
 
-const $demos = document.querySelector('#docs-demos');
-const $demo = document.querySelector('.docs-demo.is-active');
-let bounds = $demo.getBoundingClientRect();
-const refreshBounds = () => bounds = $demo.getBoundingClientRect();
-
-const circle = createAnimatable('.circle', {
-  x: 0,
-  y: 0,
-  backgroundColor: 0,
-  ease: 'outExpo',
+    render(newarr);
+  } else if (filterSelect.value == "expense") {
+    let newarr = arr.filter((ele, idx) => {
+      return ele.type == filterSelect.value;
+    });
+    render(newarr);
+  }
 });
+/// after refresh data remain there
+render();
+total();
+/// form submisiion logic
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (editingIndex !== null) {
+    arr[editingIndex].description = description.value;
+    arr[editingIndex].category = category.value;
+    arr[editingIndex].date = date.value;
+    arr[editingIndex].amount = amount.value;
+    localStorage.setItem("array", JSON.stringify(arr));
+    editingIndex = null;
+    render();
+    total();
+    btnTrans.textContent = "submit";
+  } else {
+    arr.push({
+      description: description.value,
+      category: category.value,
+      date: date.value,
+      amount: Number(amount.value),
+      type: select.value,
+    });
+    render();
+    total();
+    localStorage.setItem("array", JSON.stringify(arr));
+  }
+});
+function total() {
+  expensesTotal = arr.reduce((acc, ele, idx) => {
+    let value = ele.type == "expense" ? ele.amount : 0;
+    return acc + value;
+  }, 0);
 
-const rgb = [164, 255, 79];
-
-// Sets new durations and easings
-circle.x(0, 500, 'out(2)');
-circle.y(0, 500, 'out(3)');
-circle.backgroundColor(rgb, 250);
-
-const onMouseMove = e => {
-  const { width, height, left, top } = bounds;
-  const hw = width / 2;
-  const hh = height / 2;
-  const x = utils.clamp(e.clientX - left - hw, -hw, hw);
-  const y = utils.clamp(e.clientY - top - hh, -hh, hh);
-  rgb[0] = utils.mapRange(x, -hw, hw, 0, 164);
-  rgb[2] = utils.mapRange(x, -hw, hw, 79, 255);
-  circle.x(x).y(y).backgroundColor(rgb); // Update values
+  incomeTotal = arr.reduce((acc, ele, idx) => {
+    let value = ele.type == "income" ? ele.amount : 0;
+    return acc + value;
+  }, 0);
+  incomeD.textContent = incomeTotal;
+  expenseA.textContent = expensesTotal;
+  expenseD.textContent = expensesTotal;
+  balanceA.textContent = incomeTotal - expensesTotal;
+  balanceD.textContent = incomeTotal - expensesTotal;
+  transactions.textContent = arr.length;
 }
+//use of default params
+function render(Array = arr) {
+  trancastionList.innerHTML = "";
 
-window.addEventListener('mousemove', onMouseMove);
-$demos.addEventListener('scroll', refreshBounds);
+  Array.forEach((ele, idx) => {
+    trancastionList.innerHTML += `
+    <div class="transaction-item">
+   <div class="tx-info">
+   <span class="tx-desc">description:${ele.description}</span>
+   <span class="tx-cat"> Category:${ele.category}</span>
+   <span class="tx-date">Date:${ele.date}</span>
+   </div>
+   <div class="tx-right">
+   <span class="tx-amount ${ele.type}">Amount:${ele.amount}</span>
+   
+                </div>
+                </div>
+   <button onclick="Delete(${idx})" class="delete-btn">×</button>
+        <button onclick="Edit(${idx})"class="edit">Edit</button>
+    </div>
+          
+                `;
+    description.value = "";
+    category.value = "";
+    date.value = "";
+    amount.value = "";
+  });
+}
+let editingIndex = null;
+function Edit(edit) {
+  let idxedit = arr.find((ele, idx) => {
+    return idx == edit;
+  });
+  description.value = idxedit.description;
+  category.value = idxedit.category;
+  date.value = idxedit.date;
+  amount.value = idxedit.amount;
+
+  btnTrans.textContent = "UPDATE";
+  editingIndex = edit;
+}
+function Delete(delindx) {
+  arr = arr.filter((ele, idx) => {
+    return idx !== delindx;
+  });
+  localStorage.setItem("array", JSON.stringify(arr));
+  render();
+  total();
+}
